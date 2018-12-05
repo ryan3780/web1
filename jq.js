cadd7ebe3dd28e79858172506aade248c2f9468c
ryan3780 <45477679+ryan3780@users.noreply.github.com>
2018-12-05 20:02:47 +0900

    selectbox_edit

---
+++ jq.js
@@ -0,0 +1,66 @@
+$(function(){
+  $("#first_tag").on("change", function(){
+    var s1 = $("#second_tag");
+    var s2 = $(this).val();
+    if($(this).val() == '파트작업'){
+      typeChoice($(this).val());
+      $("#second_tag").removeAttr("disabled");
+    } 
+    else if($(this).val() == '서버작업'){
+      typeChoice1($(this).val());
+      $("#second_tag").removeAttr("disabled");
+    } 
+    else if($(this).val() == '일반업무'){
+      typeChoice2($(this).val());
+      $("#second_tag").removeAttr("disabled");
+    }
+    else if($(this).val() == '장애업무'){
+      typeChoice3($(this).val());
+      $("#second_tag").removeAttr("disabled");
+    }
+     else{
+      $("#second_tag").empty();
+      $("#second_tag").attr("disabled", "disabled");
+    } 
+  
+  });
+
+  
+  $("#second_tag").attr("disabled", "disabled");
+  
+  
+  function typeChoice(){
+      var part = ["파트 제거","파트 증설","파트 용도 변경"];
+        
+        $("#second_tag").empty();
+        for(var i=0; i< part.length; i++){
+        $("#second_tag").append('<option value="'+part[i]+'">'+part[i]+'</option>');
+      }
+  }
+   function typeChoice1(){
+      var server = ["서버 용도 변경","서버 회수","서버 투입"];
+        
+        $("#second_tag").empty();
+        for(var i=0; i< server.length; i++){
+        $("#second_tag").append('<option value="'+server[i]+'">'+server[i]+'</option>');
+      }
+  }
+   function typeChoice2(){
+      var normal = ["일반 업무"];
+        
+        $("#second_tag").empty();
+        for(var i=0; i< normal.length; i++){
+        $("#second_tag").append('<option value="'+normal[i]+'">'+normal[i]+'</option>');
+      }
+  }
+  function typeChoice3(){
+      var errors = ["파트 교체(장애)"];
+        
+        $("#second_tag").empty();
+        for(var i=0; i< errors.length; i++){
+        $("#second_tag").append('<option value="'+errors[i]+'">'+errors[i]+'</option>');
+      }
+  }
+
+
+});
