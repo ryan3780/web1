170a1dcae0bdf1122005d42d64083b0bd569e6ec
ryan3780 <45477679+ryan3780@users.noreply.github.com>
2018-12-10 15:26:58 +0900

    Update 1.php

--- 1.php
+++ 1.php
@@ -4,12 +4,20 @@ $conn = mysqli_connect(
   'root',
   '111111',
   'work_record');
-$sql = "INSERT INTO record_work (id, date, tw, kind, wm, wt, rn, wn, wnum) VALUES (NULL,'".$_POST["date"]."','".$_POST["first_tag"]."','".$_POST["second_tag"]."','".$_POST["work_num"]."','".$_POST["work_time"]."','".$_POST["req_user"]."','".$_POST["work_user"]."','".$_POST["user_num"]."')";
-//$sql = "SELECT * FROM record_work";
-$result = mysqli_query($conn, $sql);
-//$row = mysqli_fetch_array($result);
-//echo '<h1>'.$row['date'].'</h1>';
-//echo $row['tw'];
-$conn->close();
-?>
 
+$sql = "INSERT INTO record_work (id, date, tw, kind, wm, wt, rn, wn, wnum) VALUES (NULL,'".$_POST["date"]."','".$_POST["first_tag"]."','".$_POST["second_tag"]."','".$_POST["work_num"]."','".$_POST["work_time"]."','".$_POST["req_user"]."','".$_POST["work_user"]."','".$_POST["user_num"]."')";
+
+if(empty($_POST['second_tag'])){
+	
+ echo "<script>alert(\"이렇게 띄우면 되자나\");</script>";
+
+
+} else if($conn -> query($sql) === TRUE){
+
+header('Location: http://127.0.0.1');
+
+} 
+
+$conn->close();
+
+?>
